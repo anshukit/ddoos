9186b73257e89ccad4ac4bddbad6d50ef246b2d6
anshukit <63828109+anshukit@users.noreply.github.com>
2026-04-20 01:07:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-BOT_TOKEN = "8760821312:AAEQbMwKInBEqGJSQHER5MwuR_ffoqr6YzM"
+BOT_TOKEN = "8740139274:AAFcl_oR_eXcASwdcmB3NOQvewWEDv9N1FM"
 YML_FILE_PATH = ".github/workflows/main.yml"
 BINARY_FILE_NAME = "soul"
 # ADMIN_IDS = [521756472, 7733336238,7772881209] 
